7e2071f693e5378e3a161c9d6cca398e85295805
Ananth P <ananth.pattabiraman@gmail.com>
2021-06-03 23:58:01 +0530

    Scroller: improved rendering performance
    * added in-memory caching of the whole image to improve rendering
    performance. 4-times faster than before (benchmark below).
    * previous caching strategy is invoked with Caching.PNG parameter. It
    generates a full png files and reads from it for each frame. However,
    `pillow` (`PIL`) does not load the file in memory when the `Image`. This
    means, for every frame output, a disk read happens. (that's why we
    didn't bother to preserve the `Image` object).
    * new, in-memory caching is default (or with Caching.MEMORY). This loads
    the rendered PNG file into memory as numpy array using matplotlib.
        - alternative libs to load image, notably opencv, didn't support image transparency.
    
    To render the example, approx. 1 min 38 sec long 30 fps video (2926 frames):
    * Caching.MEMORY: svgvid/scroll.py  463.86s user 1.88s system 99% cpu 7:47.97 total
    * Caching.PNG:  gvid/scroll.py  1658.92s user 165.56s system 99% cpu 30:33.03 total
    
    * made the background of example svg transparent.

--- svgvid/scroll.py
+++ svgvid/scroll.py
@@ -1,11 +1,14 @@
 #!/bin/env python3
 
+from PIL import Image
 from dataclasses import dataclass
+from enum import Enum, auto
+from matplotlib import pyplot as plt
 from pathlib import Path
 import math
+import numpy as np
 import os
 import subprocess
-from PIL import Image
 import tempfile
 
 @dataclass
@@ -18,6 +21,10 @@ class Point:
         return self.x, self.y
 
 ORIGIN = Point(0, 0)
+
+class Caching(Enum):
+    MEMORY = auto(),
+    PNG = auto(),
 
 class Rect:
     def __init__(self, top_left: Point, width_px, height_px):
@@ -40,8 +47,16 @@ class Rect:
 
 def export_area(rect, source, png):
     x1, y1 = rect.top_left().unpack()
-    x2, y2 = rect.bottom_right().unpack()
-    if str(source).endswith('.png'):
+    x2, y2 = rect.bottom_right().unpack() #inclusive
+    if type(source) == np.ndarray:
+        x1 = round(x1)
+        y1 = round(y1)
+        # due to semi-open intervals in np ranges, the second point is not inclusive.
+        x2 = x1 + rect.width + 1
+        y2 = y1 + rect.height + 1
+        frame = source[y1:y2, x1:x2]
+        plt.imsave(png, frame)
+    elif str(source).endswith('.png'):
         frame = Image.open(source).crop((x1, y1, x2, y2))
         frame.save(png)
     elif str(source).endswith('.svg'):
@@ -59,30 +74,33 @@ def calculate_advance(height, pace, fps):
     return shift_px_per_sec / fps
 
 class Scroller:
-    def __init__(self, svg, outpath, frame_width_px, frame_height_px, fps, pace, enable_caching=True):
+    def __init__(self, svg, outpath, frame_width_px, frame_height_px, fps, pace, caching=Caching.MEMORY):
         """
-        svg: input SVG file path
-        outpath: directory to render output frames
-        frame_width_px, frame_height_px: width and height of the video frame
+        svg: input SVG file path.
+        outpath: directory to render output frames.
+        frame_width_px, frame_height_px: width and height of the video frame.
         bottom_px: Y coordinate of the bottom of the SVG drawing in px. Decides how far to scroll.
 
         pace: Number of seconds to scroll one screen full. float.
+        caching:
+            None - no caching. each frame gets rendered directly from svg.
+            Caching.MEMORY - default. renders the whole image once and caches in memory. fastest, but uses more memory.
+            Caching.PNG - renders the whole image once as a png file, and exports each frame from it. slightly faster than `None`.
         """
-        self.svg = Path(svg) # TODO: Validate. Here or in render?
+        self.svg = Path(svg)
         self.outpath = Path(outpath)
         self.rect = Rect(ORIGIN, frame_width_px, frame_height_px)
         self.fps = fps
-        self.enable_caching = enable_caching
+        self.caching = caching
         # number of pixels to shift down for each frame
         self.advance_px = calculate_advance(height=frame_height_px, pace=pace, fps=fps)
 
     def render(self):
         self.validate_input()
         self.create_outpath_if_required()
-        # TODO delete existing rendered frames?
 
         source = self.svg
-        if self.enable_caching:
+        if self.caching:
             tmpdir = tempfile.TemporaryDirectory()
             source = self.cache(tmpdir)
 
@@ -95,7 +113,7 @@ class Scroller:
                     source=source,
                     png=self.output_file_path(current_frame))
             self.advance()
-        
+
     def cache(self, tmpdir):
         # we have to cache the full image + one screen full of the botom portion
         # to fully scroll away the contents.
@@ -104,7 +122,11 @@ class Scroller:
         export_area(rect=Rect(ORIGIN, self.rect.width, full_height),
                 source=self.svg,
                 png=full_image_png)
-        return full_image_png
+
+        if self.caching == Caching.PNG:
+            return full_image_png
+        elif self.caching == Caching.MEMORY:
+            return plt.imread(full_image_png)
 
     def advance(self):
         self.rect.shift_down(self.advance_px)
@@ -155,6 +177,5 @@ if __name__ == '__main__':
                         outpath=rendered,
                         frame_width_px=1920, frame_height_px=1080,
                         fps=30, pace=8,
-                        enable_caching=True
                         )
     scroller.render()
